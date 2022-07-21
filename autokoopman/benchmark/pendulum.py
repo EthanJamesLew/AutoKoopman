5a7afc5b6b3f9e359ca02b35900fb3445e5c9529
Ethan Lew <elew@galois.com>
2022-07-21 09:05:02 -0700

    add docstring to pendulum with input

--- autokoopman/benchmark/pendulum.py
+++ autokoopman/benchmark/pendulum.py
@@ -4,7 +4,21 @@ import sympy as sp
 
 class PendulumWithInput(asys.SymbolicContinuousSystem):
     r"""
-    TODO: comment this
+    Simple Pendulum with Constant Torque Input
+
+        We model a pendulum with the equation:
+
+        .. math::
+            l^2 \ddot{\theta} + \beta \theta + g l \sin \theta = \tau
+
+        This leads to the state space form:
+        .. math::
+            \left\{\begin{array}{l}
+            \dot{x}_{1} = x_2 \\
+            \dot{x}_{2}= - g / l \sin x_1 - 2 \beta x_2 + u_1 \\
+            \end{array}\right.
+
+        Note that :math:`\beta=b / m` kn this formulation: http://underactuated.mit.edu/pend.html .
     """
 
     def __init__(self, g=9.81, l=1.0, beta=0.0):

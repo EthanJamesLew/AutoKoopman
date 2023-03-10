f115b239a8f44c94891fe0e0b3f4b31e52b1b138
Ethan Lew <elew@galois.com>
2023-03-09 16:50:25 -0800

    add versioneer for automatic version strings (#65)

--- autokoopman/__init__.py
+++ autokoopman/__init__.py
@@ -30,3 +30,6 @@ from autokoopman.core.trajectory import (
     UniformTimeTrajectoriesData,
     UniformTimeTrajectory,
 )
+
+from . import _version
+__version__ = _version.get_versions()['version']

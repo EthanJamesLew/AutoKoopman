d5e8447a1932ca56c2fc90dcb730d86fcb67d210
Abdu-Hekal <b6062805@ncl.ac.uk>
2023-10-29 01:27:39 +0100

    reverted back to previous tuner

--- falsification/test.py
+++ falsification/test.py
@@ -70,6 +70,8 @@ if __name__ == '__main__':
 
     params = experiment_results['hyperparameters']
     paramVals = experiment_results['hyperparameter_values']
+    score = experiment_results['tuner_score']
 
+    print(score)
     print(params)
     print(paramVals)
